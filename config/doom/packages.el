8c00d50e7d13de6ee53a8743c3958a03058d854a
sylvaster <mhxxx2005@gmail.com>
2025-10-07 13:23:11 +0200

    Add my doom emacs config

---
+++ config/doom/packages.el
@@ -0,0 +1,40 @@
+;; Org
+(package! org-pomodoro)
+(package! org-modern)
+(package! org-auto-tangle)
+(package! org-super-agenda)
+(package! consult-org-roam)
+(package! svg-tag-mode)
+
+;; Icons
+(package! nerd-icons)
+(package! nerd-icons-dired)
+(package! nerd-icons-completion)
+(package! nerd-icons-ibuffer)
+(package! all-the-icons)
+
+;; UI
+(package! rainbow-delimiters)
+(package! olivetti)
+(package! imenu-list)
+(package! focus)
+
+;; light theme
+(package! south-theme
+  :recipe (:host github
+           :repo "SophieBosio/south"))
+
+
+(package! awqat
+  :recipe (:host github
+           :repo "zkry/awqat"))
+
+;; Typst
+(package! typst-ts-mode
+  :recipe (:host nil
+           :repo "https://git.sr.ht/~meow_king/typst-ts-mode"))
+
+(package! gt)
+(package! ssh-agency)
+(package! bluetooth)
+(package! openwith)
